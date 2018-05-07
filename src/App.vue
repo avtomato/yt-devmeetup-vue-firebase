d2660e7765e83ff67414e70baf3dec5beaa4d565
avtomato <avtomato@gmail.com>
2018-05-07 08:41:46 +0300

    Dynamic Menu

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
   <v-app>
     <v-navigation-drawer temporary v-model="sideNav" fixed>
       <v-list>
-        <v-list-tile>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          router
+          :to="item.link">
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>View Meetups</v-list-tile-content>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
@@ -15,17 +19,24 @@
         @click.stop="sideNav = !sideNav"
         class="hidden-sm-and-up">
       </v-toolbar-side-icon>
-      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn flat>
-          <v-icon left dark>supervisor_account</v-icon>
-          View Meetups
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          router
+          :to="item.link">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
-
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
@@ -34,7 +45,14 @@
 export default {
   data () {
     return {
-      sideNav: false
+      sideNav: false,
+      menuItems: [
+        {icon: 'supervisor_account', title: 'View Meetups', link: '/meetups'},
+        {icon: 'room', title: 'Organize Meetup', link: '/meetup/new'},
+        {icon: 'person', title: 'Profile', link: '/profile'},
+        {icon: 'face', title: 'Sign up', link: '/signup'},
+        {icon: 'lock_open', title: 'Sign in', link: '/signin'}
+      ]
     }
   },
   name: 'App'
