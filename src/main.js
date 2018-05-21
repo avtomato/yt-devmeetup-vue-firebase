5177f6e85ec38a57d100231b7c89ebf93edcde3c
avtomato <avtomato@gmail.com>
2018-05-21 08:42:44 +0300

    Editing Date and Time

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { store } from './store'
 import DateFilter from './filters/date'
 import AlertCmp from './components/Shared/Alert'
 import EditMeetupDetailsDialog from './components/Meetup/Edit/EditMeetupDetailsDialog'
+import EditMeetupDateDialog from './components/Meetup/Edit/EditMeetupDateDialog'
+import EditMeetupTimeDialog from './components/Meetup/Edit/EditMeetupTimeDialog'
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
@@ -15,6 +17,8 @@ Vue.config.productionTip = false
 Vue.filter('date', DateFilter)
 Vue.component('app-alert', AlertCmp)
 Vue.component('app-edit-meetup-details-dialog', EditMeetupDetailsDialog)
+Vue.component('app-edit-meetup-date-dialog', EditMeetupDateDialog)
+Vue.component('app-edit-meetup-time-dialog', EditMeetupTimeDialog)
 
 /* eslint-disable no-new */
 new Vue({
